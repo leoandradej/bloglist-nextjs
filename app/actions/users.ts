6ab3cb54ae71168a89fbcd75dd165bc88dface18
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2026-05-19 23:43:11 -0300

    Fix generateToken to pass CI tests

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -2,7 +2,6 @@
 
 import bcrypt from "bcryptjs";
 import { eq } from "drizzle-orm";
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { db } from "../../db";
 import { users } from "../../db/schema";
@@ -69,5 +68,5 @@ export const generateToken = async () => {
 
   const token = `${crypto.randomUUID()}-${Date.now()}`;
   await db.update(users).set({ token }).where(eq(users.id, user.id));
-  revalidatePath("/me");
+  redirect("/me");
 };
